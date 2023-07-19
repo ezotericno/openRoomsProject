dbfb085474ac65fb0e9b8da81a66ea40d213d15b
ezotericno <65885798+ezotericno@users.noreply.github.com>
2023-07-19 09:46:19 +0000

    added test files

---
+++ Testing/test.js
@@ -0,0 +1,13 @@
+function before(){
+    document.getElementById('myImage')
+    .src="https://m.media-amazon.com/images/I/51MI37UD3nL._AC_UF894,1000_QL80_.jpg";
+    document.getElementById('message')
+    .innerHTML="Hii! GeeksforGeeks people";
+}
+     
+function afterr(){
+    document.getElementById('myImage')
+    .src="https://m.media-amazon.com/images/I/51ONUtOESOL._AC_UF350,350_QL80_.jpg";
+    document.getElementById('message')
+    .innerHTML="Bye! GeeksforGeeks people";
+}
