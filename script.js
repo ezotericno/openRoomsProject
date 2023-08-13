3c26073406c52192a222f2b2984d27a2dd489ef1
ezotericno <65885798+ezotericno@users.noreply.github.com>
2023-08-13 22:33:26 +0000

    modified files

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+function ratesD(image) {
+    if (image.src == "https://m.media-amazon.com/images/I/51ONUtOESOL._AC_UF350,350_QL80_.jpg") {
+      image.src = "https://m.media-amazon.com/images/I/51MI37UD3nL._AC_UF894,1000_QL80_.jpg";
+    } else {
+      image.src = "https://m.media-amazon.com/images/I/51ONUtOESOL._AC_UF350,350_QL80_.jpg"
+    }
+}
+
 function updateClock(){
     var now = new Date();
     var dname = now.getDay(),
