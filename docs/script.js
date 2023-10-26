d279152380e7dca6668532ec00c6e30e2c1c3453
ezotericno <65885798+ezotericno@users.noreply.github.com>
2023-10-25 23:28:49 -0400

    Update script.js
    updated file path

--- docs/script.js
+++ docs/script.js
@@ -1,8 +1,8 @@
 function ratesD(image) {
-    if (image.src == "https://m.media-amazon.com/images/I/51ONUtOESOL._AC_UF350,350_QL80_.jpg") {
-      image.src = "https://m.media-amazon.com/images/I/51MI37UD3nL._AC_UF894,1000_QL80_.jpg";
+    if (image.src == "/docs/images/closed.jpg") {
+      image.src = "/docs/images/closed.jpg";
     } else {
-      image.src = "https://m.media-amazon.com/images/I/51ONUtOESOL._AC_UF350,350_QL80_.jpg"
+      image.src = "/docs/images/closed.jpg"
     }
 }
 
